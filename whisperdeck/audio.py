710e6968de24b9e78309adb19e203970dd48c68c
Fenwick <11404933+FjgarciaMac@users.noreply.github.com>
2016-07-22 13:57:00 +0300

    chore: models

--- whisperdeck/audio.py
+++ whisperdeck/audio.py
@@ -13,3 +13,6 @@ def load_wav(path):
     """Return (sample_rate, channels, frames_as_list_of_ints)."""
     p = Path(path)
     if not p.exists():
+        raise AudioError(f"no such file: {p}")
+    with wave.open(str(p), "rb") as w:
+        rate = w.getframerate()
