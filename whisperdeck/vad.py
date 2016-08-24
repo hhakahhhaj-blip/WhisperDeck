8e820aa31ef429f29b7d6616efad831aeac8221b
Fenwick <11404933+FjgarciaMac@users.noreply.github.com>
2016-08-24 20:02:00 +0300

    chore: bump tags

--- whisperdeck/vad.py
+++ whisperdeck/vad.py
@@ -7,3 +7,5 @@ def speech_windows(path, threshold=0.04, pad_s=0.4):
     """Return [(start_s, end_s)] spans above the RMS threshold."""
     levels = rms_levels(path, window_s=0.25)
     windows = []
+    open_at = None
+    for i, level in enumerate(levels):
