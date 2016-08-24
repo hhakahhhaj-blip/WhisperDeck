32f648a52c57958fd8a6925b9bc62bd31301012a
Fenwick <11404933+FjgarciaMac@users.noreply.github.com>
2016-08-24 19:37:00 +0300

    style: clean up exports

--- whisperdeck/vad.py
+++ whisperdeck/vad.py
@@ -6,3 +6,4 @@ from .audio import rms_levels
 def speech_windows(path, threshold=0.04, pad_s=0.4):
     """Return [(start_s, end_s)] spans above the RMS threshold."""
     levels = rms_levels(path, window_s=0.25)
+    windows = []
