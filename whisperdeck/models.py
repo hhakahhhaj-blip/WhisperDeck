a4cab32efa6c4775cee24051b29886dc8a87391c
Fenwick <11404933+FjgarciaMac@users.noreply.github.com>
2018-01-06 15:21:00 +0200

    fix: correct whisper

--- whisperdeck/models.py
+++ whisperdeck/models.py
@@ -7,3 +7,6 @@ REGISTRY = {
     "base.en":   {"size_mb": 142,  "params": "74M",   "vram_mb": 500},
     "small.en":  {"size_mb": 466,  "params": "244M",  "vram_mb": 1000},
     "medium.en": {"size_mb": 1500, "params": "769M",  "vram_mb": 2800},
+    "large-v3":  {"size_mb": 2900, "params": "1550M", "vram_mb": 5200},
+}
+
