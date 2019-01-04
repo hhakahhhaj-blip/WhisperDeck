509113718bc2ae1f5ec106f2cbdef0fa393d4261
Fenwick <11404933+FjgarciaMac@users.noreply.github.com>
2019-01-04 10:03:00 +0200

    refactor: exports

--- whisperdeck/cli.py
+++ whisperdeck/cli.py
@@ -7,3 +7,7 @@ from .audio import duration_seconds
 from .tags import read_tags
 from .deck import Deck
 from .exports import export_deck
+
+
+def main(argv=None):
+    ap = argparse.ArgumentParser(prog="wd")
