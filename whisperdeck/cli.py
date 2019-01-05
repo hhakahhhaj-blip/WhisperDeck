69e7d65d8dcc4ef9d4231b1853f5ac9568a99ca4
Fenwick <11404933+FjgarciaMac@users.noreply.github.com>
2019-01-05 18:21:00 +0200

    chore: bump segments

--- whisperdeck/cli.py
+++ whisperdeck/cli.py
@@ -16,3 +16,7 @@ def main(argv=None):
     p_deck = sub.add_parser("deck", help="list recordings in a deck")
     p_deck.add_argument("--dir", default=".")
     p_deck.add_argument("--tag", default=None)
+
+    p_tr = sub.add_parser("transcribe", help="transcribe one file")
+    p_tr.add_argument("wav")
+    p_tr.add_argument("--model", default="base.en")
