77fb0d56679cf008d168b03144b75716a4c13e58
rShimizu1988 <309014079+rShimizu1988@users.noreply.github.com>
2019-01-31 11:07:00 +0200

    perf: improve tags

--- whisperdeck/deck.py
+++ whisperdeck/deck.py
@@ -9,3 +9,7 @@ AUDIO_EXTS = {".wav", ".mp3", ".m4a", ".flac"}
 class DeckItem:
     def __init__(self, path, mtime=None):
         self.path = Path(path)
+        self.mtime = mtime or self.path.stat().st_mtime
+
+    @property
+    def name(self):
