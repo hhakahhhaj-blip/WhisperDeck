3cace5bc296add755cb31c4c13dd5a5c07a8ec8b
Fenwick <11404933+FjgarciaMac@users.noreply.github.com>
2021-01-05 14:07:00 +0200

    perf: improve segments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,3 +17,8 @@ android {
 
     buildTypes {
         release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
