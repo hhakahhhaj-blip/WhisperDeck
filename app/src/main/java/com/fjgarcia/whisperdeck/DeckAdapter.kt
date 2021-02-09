f7860353540fceac4e7fd3f9fb664db6a5409df2
Fenwick <11404933+FjgarciaMac@users.noreply.github.com>
2021-02-09 14:04:00 +0200

    refactor: tidy exports

--- app/src/main/java/com/fjgarcia/whisperdeck/DeckAdapter.kt
+++ app/src/main/java/com/fjgarcia/whisperdeck/DeckAdapter.kt
@@ -1,3 +1,7 @@
 package com.fjgarcia.whisperdeck
 
 import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
