256b950bbcdc9ab640be47e0fbd74b90c3f3ad47
rShimizu1988 <309014079+rShimizu1988@users.noreply.github.com>
2021-02-09 14:07:00 +0200

    build: android

--- app/src/main/java/com/fjgarcia/whisperdeck/DeckAdapter.kt
+++ app/src/main/java/com/fjgarcia/whisperdeck/DeckAdapter.kt
@@ -5,3 +5,8 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
+data class DeckItem(val name: String, val ageDays: Int)
+
+class DeckAdapter(private val onClick: (DeckItem) -> Unit) :
+    RecyclerView.Adapter<DeckAdapter.Holder>() {
+
