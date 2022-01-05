ac202e9a9584a44259b46f9814514eefb18b512e
Fenwick <11404933+FjgarciaMac@users.noreply.github.com>
2022-01-05 10:15:00 +0200

    refactor: exports

--- app/src/main/java/com/fjgarcia/whisperdeck/MainActivity.kt
+++ app/src/main/java/com/fjgarcia/whisperdeck/MainActivity.kt
@@ -3,3 +3,8 @@ package com.fjgarcia.whisperdeck
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
