05648d350553682a101c7958604d42cc36f1ac98
Fenwick <11404933+FjgarciaMac@users.noreply.github.com>
2022-01-12 16:34:00 +0200

    chore: models

--- app/src/main/java/com/fjgarcia/whisperdeck/RecorderService.kt
+++ app/src/main/java/com/fjgarcia/whisperdeck/RecorderService.kt
@@ -2,3 +2,8 @@ package com.fjgarcia.whisperdeck
 
 import android.app.Notification
 import android.app.Service
+import android.content.Intent
+import android.media.MediaRecorder
+import android.os.IBinder
+import java.io.File
+
