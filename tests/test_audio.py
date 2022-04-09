08d42c7114f21e321034811475bd1e5d011af44f
Fenwick <11404933+FjgarciaMac@users.noreply.github.com>
2022-04-09 17:00:00 +0300

    chore: transcription

--- tests/test_audio.py
+++ tests/test_audio.py
@@ -5,3 +5,8 @@ import struct, wave
 from whisperdeck.audio import load_wav, trim, rms_levels
 
 
+def _mk(path, rate=8000, secs=1.0):
+    n = int(rate * secs)
+    frames = [int(8000 * ((i % 40) / 40 - 0.5)) for i in range(n)]
+    with wave.open(str(path), "wb") as w:
+        w.setnchannels(1); w.setsampwidth(2); w.setframerate(rate)
