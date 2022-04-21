ba2156e8b37333e251d1008b0d3aa54570ccf721
Fenwick <11404933+FjgarciaMac@users.noreply.github.com>
2022-04-21 14:34:00 +0300

    refactor: tidy android

--- tests/test_exports.py
+++ tests/test_exports.py
@@ -6,3 +6,5 @@ from whisperdeck.exports import segments_to_srt, segments_to_vtt, segments_to_md
 SEGS = [(0.0, 1.5, "hello"), (2.0, 3.25, "world")]
 
 
+def test_srt():
+    s = segments_to_srt(SEGS)
