bc9f33a5390affc78a729ec87a8a6c15264cbaee
Fenwick <11404933+FjgarciaMac@users.noreply.github.com>
2022-05-01 15:08:00 +0300

    docs: document whisper

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -5,3 +5,6 @@ from whisperdeck.tags import read_tags, write_tags, add_tag, filter_by_tag
 
 def test_roundtrip(tmp_path):
     p = tmp_path / "memo.wav"
+    p.write_bytes(b"RIFF")
+    write_tags(p, ["podcast", "draft"], notes="first pass")
+    data = read_tags(p)
