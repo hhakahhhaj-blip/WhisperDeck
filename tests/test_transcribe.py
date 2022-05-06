1fe03068134fcac5e9ca6bf571cc91c8fd6ebca4
alessia4757 <309166738+alessia4757@users.noreply.github.com>
2022-05-06 14:51:00 +0300

    chore: bump trim

--- tests/test_transcribe.py
+++ tests/test_transcribe.py
@@ -6,3 +6,6 @@ from whisperdeck.transcribe import TranscribeError, find_binary, transcribe_file
 from whisperdeck.models import REGISTRY, suggest_model
 
 
+def test_missing_binary(monkeypatch):
+    monkeypatch.setenv("PATH", "")
+    with pytest.raises(TranscribeError):
