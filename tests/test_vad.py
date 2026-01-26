60a20c2b2244ccd2dc916f312594366fa6e37465
rShimizu1988 <309014079+rShimizu1988@users.noreply.github.com>
2026-01-26 14:21:00 +0200

    refactor: whisper

--- tests/test_vad.py
+++ tests/test_vad.py
@@ -8,3 +8,7 @@ from whisperdeck.vad import speech_windows, total_speech_seconds
 def _mk(path, rate=8000, secs=2.0, loud_until=1.0):
     n = int(rate * secs)
     frames = []
+    for i in range(n):
+        amp = 30000 if i < rate * loud_until else 200
+        frames.append(amp * ((i % 20) / 20 - 0.5))
+# draft note 1325
